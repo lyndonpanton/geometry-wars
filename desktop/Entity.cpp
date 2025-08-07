6f43f91d6a3a5191d67164fac7755cc1649d7e33
Lyndon Panton <LyndonPanton@gmail.com>
2025-08-08 00:38:57 +0100

    Setup: Recreate Entity class

---
+++ desktop/Entity.cpp
@@ -0,0 +1,62 @@
+#include "Entity.h"
+
+// constructors
+
+// getters
+bool Entity::getIsActive() const
+{
+	return m_is_active;
+}
+
+size_t Entity::getId() const
+{
+	return m_id;
+}
+
+const std::string& Entity::getTag() const
+{
+	return m_tag;
+}
+
+// setters
+void Entity::destroy()
+{
+	m_is_active = false;
+}
+
+// overloads
+
+// other
+template <typename T, typename... TArgs>
+T& Entity::add(TArgs&&... m_args)
+{
+	auto component = get<T>();
+	component = T(std::forward<TArgs>(m_args));
+	component.exists = true;
+
+	return component;
+}
+
+template <typename T>
+T& Entity::get()
+{
+	return std::get<T>(m_components);
+}
+
+template <typename T>
+const T& Entity::get() const
+{
+	return std::get<T>(m_components);
+}
+
+template <typename T>
+bool Entity::has() const
+{
+	return get<T>().exists;
+}
+
+template <typename>
+void Entity::remove()
+{
+	get<T>() = T();
+}
